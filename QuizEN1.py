21f827a44b389662fb0cf076693afe3553ddd9ca
EricNiyazov <45433844+EricNiyazov@users.noreply.github.com>
2019-01-16 12:59:02 -0500

    custom function defined, score commented out and 2 questions working with custom function, no score

--- QuizEN1.py
+++ QuizEN1.py
@@ -4,6 +4,28 @@ grade = 0 # grade is 0
 a1 = 0
 a2 = 0
 
+
+
+def ask_and_check(qText, qChk, aUser, aCorr):
+    print(qText) #print question1
+
+    while qChk == 0: #while test is false run loop
+        try:
+            aUser = int(input(">>"))#answer one equal an integer input
+            if aUser == aCorr:# if answer 1 equal 1
+                print ("Ok I got it")#tell user ok answer
+                #grade += 1#change grade by one
+                qChk = 1#escape condish
+            elif 4 >= aUser >= 1:
+                print("Ok I got it")#tell user ok answer
+                qChk = -1 #escape condish
+            elif aUser > 4: #if answer more then 4
+                print ("no answers more then 4 duh") # tell user no answer more then 4
+            else:
+                print ("no answers less then 1 duh") #tell user no answer less then 1
+        except ValueError: #if ValueError
+            print("answer must be a integer 1,2,3 or 4")   #tell user it must be int
+
 q1 = """
 What's 9 + 10?
 1. 19 
@@ -18,40 +40,6 @@ What is the meaning of life the universe and everything?
 3. there is no meaning :(
 4. 42"""
 
-print (q1)
-while q1chk == 0: #while test is false run loop
-    try:
-        a1 = int(input(" "))#answer one equal an integer input
-        if a1 == 1:# if answer 1 equal 1
-            print ("Ok I got it")#tell user ok answer
-            grade += 1#change grade by one
-            q1chk = 1#escape condish
-        elif 4 >= a1 > 1:
-            print("Ok I got it")#tell user ok answer
-            q1chk = -1 #escape condish
-        elif a1 > 4: #if answer more then 4
-            print ("no answers more then 4 duh") # tell user no answer more then 4
-        else:
-            print ("no answers less then 1 duh") #tell user no answer less then 1
-    except ValueError: #if ValueError
-        print("answer must be a integer 1,2,3 or 4")   #tell user it must be int
+ask_and_check(q1, q1chk, a1, 1)
+ask_and_check(q2, q2chk, a2, 4)
 
-print (q2)
-while q2chk == 0: #while test is false run loop
-    try:
-        a2 = int(input(" "))#answer one equal an integer input
-        if a2 == 4:# if answer 1 equal 1
-            print ("Ok I got it")#tell user ok answer
-            grade += 1#change grade by one
-            q2chk = 1#escape condish
-        elif 4 >= a2 > 1:
-            print("Ok I got it")#tell user ok answer
-            q2chk = -1 #escape condish
-        elif a2 > 4: #if answer more then 4
-            print ("no answers more then 4 duh") # tell user no answer more then 4
-        else:
-            print ("no answers less then 1 duh") #tell user no answer less then 1
-    except ValueError: #if ValueError
-        print("answer must be a integer 1,2,3 or 4")   #tell user it must be int
-
-print("your grade is",grade,) #print the grade
